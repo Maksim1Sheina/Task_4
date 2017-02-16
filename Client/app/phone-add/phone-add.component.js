c4212b50747f3bdc7f46279a99b2ba0f5c6e9742
Maksim1Sheina <hai_power@mail.ru>
2017-02-16 14:02:07 +0300

    Add sending some data

--- Client/app/phone-add/phone-add.component.js
+++ Client/app/phone-add/phone-add.component.js
@@ -10,8 +10,49 @@ angular.
                 var phone;
                 
                 self.btnClick = function btnClick(){
-                    self.phone.$save();
+                    var outData = new Phone();
+                    outData.Name = self.phone.Name;
+                    outData.Description = self.phone.Description;
+                    outData.$save();
                 };
             }
         ]
 });
+
+/*
+'use strict';
+
+angular.
+    module('phoneAdd').
+    component('phoneAdd', {
+        templateUrl: 'phone-add/phone-add.template.html',
+        controller: ['Phone',
+            function PhoneAddController(Phone, $http){
+                var self = this;
+                var phone;
+                var res = false;
+                
+                self.btnClick = function btnClick(){
+                    self.phone.$save();
+                };
+                
+                if(res){
+                    $http.post('http://localhost:4871/api/Phones', phone, config)
+                        .then(function successCallback(response) {
+                                // this callback will be called asynchronously
+                                // when the response is available
+                                alert("Data delivered successfully");
+                              }, function errorCallback(response) {
+                                // called asynchronously if an error occurs
+                                // or server returns response with an error status.
+                                alert("Error with data sending");
+                              });
+                }
+                
+                self.btnClick = function btnClick(){
+                    self.res = true;
+                };
+            }
+        ]
+});
+*/
