343a3b41631f5903eedae233a2a49837b5a05550
Maksim1Sheina <hai_power@mail.ru>
2017-02-17 10:59:41 +0300

    Add_adding(in_work)

--- Client/app/phone-add/phone-add.component.js
+++ Client/app/phone-add/phone-add.component.js
@@ -9,50 +9,76 @@ angular.
                 var self = this;
                 var phone;
                 
+                self.Name = 'No data';
+                self.Description = 'No data';
+                self.Availabilities = ['No data', 'no data'];
+                self.BatteryType = 'No data';
+                self.BatteryTalkTime = 'No data';
+                self.BatteryStandbyTime = 'No data';
+                self.StorageRAM = 64;
+                self.StorageFlash = 64;
+                self.ConnectivityCell = 'No data';
+                self.ConnectivityWiFi = 'No data';
+                self.ConnectivityBluetooth = 'No data';
+                self.ConnectivityInfrared = false;
+                self.ConnectivityGPS = false;
+                self.PlatformType = 'No data';
+                self.PlatrormVersion = 'No data';
+                self.PlatformUI = 'No data';
+                self.Width = 0;
+                self.Height = 0;
+                self.Depth = 0;
+                self.Weight = 0;
+                self.DisplayScreenSize = 'No data';
+                self.DisplayScreenResolution = 'No data';
+                self.DisplayTouchScreen = false;
+                self.HardwareCPU = 'No data';
+                self.HardwareUSB = 'No data';
+                self.HardwareAudioJack = 'No data';
+                self.HardwareFMRadio = false;
+                self.HardwareAccelerometer = false;
+                self.HardwarePhysicalKeyboard = false;
+                self.CameraPrimary = 0;
+                self.CameraFeatures = ['No data', 'no data'];
+                self.AdditionalFeatures = 'No data';
+                
                 self.btnClick = function btnClick(){
                     var outData = new Phone();
-                    outData.Name = self.phone.Name;
-                    outData.Description = self.phone.Description;
+                    outData.Name = self.Name;
+                    outData.Description = self.Description;
+                    outData.Availabilities = self.Availabilities;
+                    outData.BatteryType = self.BatteryType;
+                    outData.BatteryTalkTime = self.atteryTalkTime;
+                    outData.BatteryStandbyTime = self.BatteryStandbyTime;
+                    outData.StorageRAM = self.StorageRAM;
+                    outData.StorageFlash = self.StorageFlash;
+                    outData.ConnectivityCell = self.ConnectivityCell;
+                    outData.ConnectivityWiFi = self.ConnectivityWiFi;
+                    outData.ConnectivityBluetooth = self.ConnectivityBluetooth;
+                    outData.ConnectivityInfrared = self.ConnectivityInfrared;
+                    outData.ConnectivityGPS = self.ConnectivityGPS;
+                    outData.PlatformType = self.PlatformType;
+                    outData.PlatrormVersion = self.PlatrormVersion;
+                    outData.PlatformUI = self.PlatformUI;
+                    outData.Width = self.Width;
+                    outData.Height = self.Height;
+                    outData.Depth = self.Depth;
+                    outData.Weight = self.Weight;
+                    outData.DisplayScreenSize = self.DisplayScreenSize;
+                    outData.DisplayScreenResolution = self.DisplayScreenResolution;
+                    outData.DisplayTouchScreen = self.DisplayTouchScreen;
+                    outData.HardwareCPU = self.HardwareCPU;
+                    outData.HardwareUSB = self.HardwareUSB;
+                    outData.HardwareAudioJack = self.HardwareAudioJack;
+                    outData.HardwareFMRadio = self.HardwareFMRadio;
+                    outData.HardwareAccelerometer = self.HardwareAccelerometer;
+                    outData.HardwarePhysicalKeyboard = self.HardwarePhysicalKeyboard;
+                    outData.CameraPrimary = self.CameraPrimary;
+                    outData.CameraFeatures = self.CameraFeatures;
+                    outData.AdditionalFeatures = self.AdditionalFeatures;
+                    
                     outData.$save();
                 };
             }
         ]
 });
-
-/*
-'use strict';
-
-angular.
-    module('phoneAdd').
-    component('phoneAdd', {
-        templateUrl: 'phone-add/phone-add.template.html',
-        controller: ['Phone',
-            function PhoneAddController(Phone, $http){
-                var self = this;
-                var phone;
-                var res = false;
-                
-                self.btnClick = function btnClick(){
-                    self.phone.$save();
-                };
-                
-                if(res){
-                    $http.post('http://localhost:4871/api/Phones', phone, config)
-                        .then(function successCallback(response) {
-                                // this callback will be called asynchronously
-                                // when the response is available
-                                alert("Data delivered successfully");
-                              }, function errorCallback(response) {
-                                // called asynchronously if an error occurs
-                                // or server returns response with an error status.
-                                alert("Error with data sending");
-                              });
-                }
-                
-                self.btnClick = function btnClick(){
-                    self.res = true;
-                };
-            }
-        ]
-});
-*/
